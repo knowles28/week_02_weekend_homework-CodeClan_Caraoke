093762af53e5365b918ae9847b71d567c3b28fe7
Jack Knowles <jackmhknowles@gmail.com>
2022-07-01 11:44:25 +0100

    ran first test on song

--- tests/song_test.py
+++ tests/song_test.py
@@ -2,4 +2,9 @@ import unittest
 from src.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.song = Song("Wonderwall_Oasis")
+        
+    def test_song_has_name(self):
+        self.assertEqual("Wonderwall_Oasis", self.song.name)
