b84d36bd4a846d93378fe519f0aaa9dca99c328b
Jack Knowles <jackmhknowles@gmail.com>
2022-07-03 15:21:45 +0100

    updated guest capacity tests for rooms

--- src/room.py
+++ src/room.py
@@ -1,16 +1,20 @@
 class Room:
-    def __init__(self, _room_name, _capacity, _till):
+    def __init__(self, _room_name, _capacity):
         self.room_name = _room_name
         self.capacity = _capacity
         self.playlist = {}
         self.guests = []
-        self.room_till = _till
+        self.room_till = 0
     
     def check_capacity(self):
         return len(self.guests)
     
     def add_guest_to_room(self, guest):
-        self.guests.append(guest)
+        if self.check_capacity() < self.capacity:
+            self.guests.append(guest)
+        else:
+            return f"{self.room_name} is at full capacity! The capcity for this room is {self.capacity} guests."
+
         
     def remove_guest_from_room(self, guest):
         self.guests.remove(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,13 +7,22 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
-        self.room_1 = Room("Room A", 8, 0)
-        self.guest_1 = Guest("Paul", 10, "In Da Club")
+        self.room_1 = Room("Room A", 4)
+        self.room_2 = Room("Room B", 2)
+        
+        self.guest_1 = Guest("Paul", 10, "Hey Jude")
+        self.guest_2 = Guest("John", 5, "Let It Be")
+        self.guest_3 = Guest("Ringo", 20, "Help!")
+        
         self.song_1 = Song("Signs", "Snoop Dogg")
+        self.song_2 = Song("One More Time", "Daft Punk")
 
     
     def test_room_has_name(self):
         self.assertEqual("Room A", self.room_1.room_name)
+        
+    def test_room_has_capacity_limit(self):
+        self.assertEqual(4, self.room_1.capacity)
     
     def test_add_guest_to_room(self):
         self.room_1.add_guest_to_room(self.guest_1)
@@ -23,7 +32,26 @@ class TestRoom(unittest.TestCase):
         self.room_1.add_guest_to_room(self.guest_1)
         self.room_1.remove_guest_from_room(self.guest_1)
         self.assertEqual(0, self.room_1.check_capacity())
+        
+    def test_room_allows_guests_within_capcacity(self):
+        self.room_1.add_guest_to_room(self.guest_1)
+        self.room_1.add_guest_to_room(self.guest_2)
+        self.room_1.add_guest_to_room(self.guest_3)
+        self.assertGreaterEqual(self.room_1.capacity, self.room_1.check_capacity())
+    
+    def test_room_rejects_guests_over_capacity(self):
+        self.room_2.add_guest_to_room(self.guest_1)
+        self.room_2.add_guest_to_room(self.guest_2)
+        self.room_2.add_guest_to_room(self.guest_3)
+        self.assertEqual("Room B is at full capacity! The capcity for this room is 2 guests.", self.room_2.add_guest_to_room(self.guest_3))
   
     def test_add_song_to_playlist(self):
         self.room_1.add_song_to_playlist(self.song_1.name, self.song_1.artist)
         self.assertEqual({self.song_1.name: self.song_1.artist}, self.room_1.playlist)
+        
+    def test_add_multiple_songs_to_playlist(self):
+        self.room_1.add_song_to_playlist(self.song_1.name, self.song_1.artist)
+        self.room_1.add_song_to_playlist(self.song_2.name, self.song_2.artist)
+        self.assertEqual({self.song_1.name: self.song_1.artist, self.song_2.name: self.song_2.artist}, self.room_1.playlist)
+        
+        
